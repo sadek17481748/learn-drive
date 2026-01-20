6bb14cd07c289b6557ce7fbdd78b23b8db40a65a
eventiservicesandhelp-ops <eventiservicesandhelp-ops@users.noreply.github.com>
2026-01-20 20:22:26 +0000

    Add shuffle helper function for randomizing quiz options

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -159,7 +159,16 @@ function restartQuiz() {
   
     // Reset hazard perception video to first video
     loadHazardVideo(0);
+  } 
+  // ================= SHUFFLE HELPER =================
+// Randomly shuffles an array in place
+function shuffle(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
   }
+  
   
   
   
